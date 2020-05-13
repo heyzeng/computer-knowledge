dfc9911837c153c6245f6c8da12c8647039f1875
heyzeng <270806204@qq.com>
2020-05-13 20:56:45 +0800

    sparktest

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,9 @@ F
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 g
 7scala-learning/src/main/scala/com/chapter01/Hello.scala,4/9/490a1cc776609375e52e13c9056cea130f679f34
+G
+spark-streaming/pom.xml,b/2/b2e0f3b6b8711e122076a4e5472e15141466ef92
+A
+spark-sql/pom.xml,b/4/b409298fa74d5e2c3bd75dbcce936ad05e8a314a
+B
+spark-core/pom.xml,b/f/bf90affb51be30f2507e68784de2dc5118880cba
---
+++ spark-core/src/main/scala/chapter01/sparkTest.scala
@@ -0,0 +1,32 @@
+package chapter01
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Author:Jude 
+  * Date:2020-05-13 8:37 PM 
+  */
+object sparkTest {
+  def main(args: Array[String]): Unit = {
+
+    // 创建sparkContext
+    val conf = new SparkConf().setAppName("sparkTest").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+
+    // 创建RDD
+    val lines = sc.textFile("/Users/judezeng/Desktop/GoodGoodStudy/scala/spark-core/src/main/resources/a.txt")
+
+    // 转换算子操作
+    val data = lines.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+
+    //  对RDD做行动操作,把计算结果拉取到驱动端
+    val result = data.collect()
+    result.foreach(print)
+
+    // 停止SparkContext
+    sc.stop()
+
+
+  }
+
+}
