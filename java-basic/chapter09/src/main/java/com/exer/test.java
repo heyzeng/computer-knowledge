04b0d4c0ed47df236a98632b233c5d1b2faa68a7
zengjf <270806204@qq.com>
2020-07-27 15:54:41 +0800

    update

--- java-basic/chapter09/src/main/java/com/exer/test.java
+++ java-basic/chapter09/src/main/java/com/exer/test.java
@@ -1,9 +1,35 @@
 package com.exer;
 
+import java.util.*;
 
 public class test {
     public static void main(String[] args) {
 
-        System.out.println(6|3);
+  //      System.out.println(6|3);
+
+        HashMap<String, String> map = new HashMap<>();
+
+        Scanner sc = new Scanner(System.in);
+        int i =  0;
+
+        while (i < 3){
+
+            String key = sc.next();
+            String value = sc.next();
+            map.put(key,value);
+            i++;
+        }
+        Iterator<String> it = map.values().iterator();
+        while (it.hasNext()){
+            System.out.println(it.next() + " --");
+        }
+
+        //使用entrySet方法获取key-value值
+        Set<Map.Entry<String,String>> set=map.entrySet();
+        for(Map.Entry<String,String> entry:set) {
+            System.out.print(entry.getKey()+"-");
+            System.out.println(entry.getValue());
+        }
+
     }
 }
