407ddb26aab13ea9b553aeeb66702eac2a04675c
zengjf <270806204@qq.com>
2020-08-26 12:16:57 +0800

    chapter02

--- java-basic/chapter02/src/main/java/AddTest.java
+++ java-basic/chapter02/src/main/java/AddTest.java
@@ -1,8 +1,15 @@
+import java.util.Random;
 
 public class AddTest {
     public static void main(String[] args) {
 
         //hello1003
         System.out.println("hello" + 100 +3);
-    }
+
+
+        // 60% 的概率输出 YES，40% 的概率输出 NO
+        System.out.println((new Random().nextInt() % 100) > 60 ? "YES" : "NO");
+            }
+
+
 }
