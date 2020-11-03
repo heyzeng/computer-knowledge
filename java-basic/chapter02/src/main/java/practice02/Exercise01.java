be00759f8db361f8d7861471f910f155ba0a14f1
heyzeng <270806204@qq.com>
2020-11-03 17:52:28 +0800

    update

---
+++ java-basic/chapter02/src/main/java/practice02/Exercise01.java
@@ -0,0 +1,18 @@
+package practice02;
+
+
+public class Exercise01 {
+    public static void main(String[] args) {
+
+        //
+        int i = 1;
+        i= i++;
+        System.out.println(i);//i=1
+
+        //
+        int j = 1;
+        j= ++j;
+        System.out.println(j);//i=2
+
+    }
+}
