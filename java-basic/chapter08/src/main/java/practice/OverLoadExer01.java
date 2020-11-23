17d7463f07cae6af83c837efdd08e457baf815a5
heyzeng <270806204@qq.com>
2020-11-23 19:29:41 +0800

    overload

--- java-basic/chapter08/src/main/java/practice/OverLoadExer01.java
+++ java-basic/chapter08/src/main/java/practice/OverLoadExer01.java
@@ -8,6 +8,10 @@ public class OverLoadExer01 {
 
     public static void main(String[] args) {
 
+        MethodUtils methodUtils = new MethodUtils();
+        System.out.println(methodUtils.max(2,3));
+        System.out.println(methodUtils.max(2.1,2.5));
+        System.out.println(methodUtils.max(2.1,4,8.1));
     }
 }
 
@@ -15,4 +19,19 @@ public class OverLoadExer01 {
 //第一个方法，返回两个int值中的最大值，
 //第二个方法，返回两个double值中的最大值，
 //第三个方法，返回三个double值中的最大值，并分别调用三个方法。
+
+class MethodUtils{
+
+    public int max(int a, int b){
+        return a > b ? a :b;
+    }
+
+    public double max (double a,double b){
+        return a > b ? a : b;
+    }
+
+    public double max(double a,double b, double c){
+        return (a > b ? a : b) > c ? (a > b ? a : b) : c;
+    }
+}
 
