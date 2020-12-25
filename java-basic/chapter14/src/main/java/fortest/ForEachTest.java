3e22f2f2f57389b0ab861603bc1fc287549e069c
heyzeng <270806204@qq.com>
2020-12-25 10:41:57 +0800

    fortest

---
+++ java-basic/chapter14/src/main/java/fortest/ForEachTest.java
@@ -0,0 +1,26 @@
+package fortest;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * Author:ZJF
+ * Date:2020-12-25 上午10:35
+ */
+public class ForEachTest {
+
+    public static void main(String[] args) {
+
+        int[] a = {1,3,4};
+        for (int i:a) {//增强for循环
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+        //Arrays.toString
+        System.out.println(Arrays.toString(a));//打印数组[1, 3, 4]
+    }
+}
