b346131d7f7f923d9d2a8ce860ceb2721ab08869
heyzeng <270806204@qq.com>
2020-12-31 16:41:30 +0800

    hashmap ergodic

--- java-basic/chapter18/src/main/java/map/HashMapForeach.java
+++ java-basic/chapter18/src/main/java/map/HashMapForeach.java
@@ -18,16 +18,15 @@ public class HashMapForeach {
         map.put("宋喆", "马蓉");
         map.put("刘令博", null);
         map.put(null, "刘亦菲");
-
         map.put("鹿晗", "关晓彤");
 
         System.out.println("-----------one---------------");
         // first ergodic
         Set set = map.keySet();
         Iterator iterator = set.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             Object key = iterator.next();
-            System.out.println(key + " " + map.get(key) );
+            System.out.println(key + " " + map.get(key));
         }
 
         System.out.println("-----------two---------------");
@@ -40,17 +39,17 @@ public class HashMapForeach {
         //third ergodic
         Set entrySet = map.entrySet();//1. 把所有的k-v(entry) , 放入到一个集合
         Iterator iterator1 = entrySet.iterator();
-        while (iterator1.hasNext()){
+        while (iterator1.hasNext()) {
             //1. entrySet 中存放的元素 的运行类型是 HashMap$Node
             //Object object =  iterator2.next();
-            Map.Entry node = (Map.Entry)iterator1.next();
-            System.out.println(node.getKey() + " " + node.getValue() );
+            Map.Entry node = (Map.Entry) iterator1.next();
+            System.out.println(node.getKey() + " " + node.getValue());
         }
 
         System.out.println("-----------four---------------");
         //fourth ergodic
-        for (Object entry: entrySet) {
-            Map.Entry node = (Map.Entry)entry;//obj 的编译类型Object, obj运行类型 HashMap$Node
+        for (Object entry : entrySet) {
+            Map.Entry node = (Map.Entry) entry;//obj 的编译类型Object, obj运行类型 HashMap$Node
             System.out.println(node.getKey() + " " + node.getValue());
         }
 
