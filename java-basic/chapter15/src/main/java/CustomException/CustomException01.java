52eed65a48d9ce2f38674b18fa3e7fa8935127b9
heyzeng <270806204@qq.com>
2021-02-22 19:27:22 +0800

    customException

--- java-basic/chapter15/src/main/java/CustomException/CustomException01.java
+++ java-basic/chapter15/src/main/java/CustomException/CustomException01.java
@@ -6,6 +6,27 @@ package CustomException;
  */
 public class CustomException01 {
     public static void main(String[] args) {
-        
+
+        int age = 200;
+
+        try {
+            if (!(age >= 1 && age <= 100)){
+                throw new AgeException("不合理");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
+
+//自定义异常
+class AgeException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public AgeException(String message) {
+        super(message);
+    }
+}
+
