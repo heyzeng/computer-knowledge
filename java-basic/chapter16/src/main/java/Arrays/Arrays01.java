f31229889789bbfcf2d31bfd8af3cb9a8fe32407
zengjf <270806204@qq.com>
2021-03-26 11:02:12 +0800

    remove file

--- java-basic/chapter16/src/main/java/Arrays/Arrays01.java
+++ java-basic/chapter16/src/main/java/Arrays/Arrays01.java
@@ -1,12 +1,33 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Author:ZJF
  * Date:2021-03-21 上午12:10
  */
 public class Arrays01 {
     public static void main(String[] args) {
+        Integer[] arr = {1,4,2,5};
 
+        Arrays.sort(arr, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Integer n1 = (Integer) o1;
+                Integer n2 = (Integer) o2;
+
+                if (n1 < n2) {
+                    return -1;
+                }else if (n1 > n2){
+                    return 1;
+                }else {
+                    return 0;
+                }
+            }
+        });
+
+        System.out.println(Arrays.toString(arr));
     }
 }
 
