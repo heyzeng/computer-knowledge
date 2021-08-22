ada46881cd03384659ca246dbedf84919eb1108d
heyzeng <270806204@qq.com>
2021-08-22 11:12:48 +0800

    java

---
+++ mysql/src/main/java/test.java
@@ -0,0 +1,24 @@
+/**
+ * Author:Jude
+ * Date:2021-08-17 下午8:54
+ */
+
+/**
+ * 给定任意一个整型数组，里面的值有正有负。求最大的连加值，比如3、-6、1、2、3、-1、2、-5、1、2，这个数组的最大子数组就是1、2、3、-1、2，所以最大连加值是7
+ */
+public class test {
+    public static void main(String[] args) {
+        int[] arr = {3,-6,1,2,3,-1,2,-5,1,2};
+
+        int sum = 0;
+
+        int[] arr2;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+        }
+
+
+    }
+
+}
+
