3817ddd1848acf5428a57a758d8ab731f5e48851
heyzeng <270806204@qq.com>
2021-12-12 23:26:37 +0800

    sqr

---
+++ leetcode/src/main/java/lc_0028_strStr.java
@@ -0,0 +1,18 @@
+/**
+ * Author:Jude
+ * Date:2021-12-12 下午11:06
+ */
+
+/**
+ * KMP算法。求最长公共子序列
+ */
+public class lc_0028_strStr {
+    public static void main(String[] args) {
+
+    }
+
+    // 1 暴力匹配
+    public static int strStrViolentMatch (String str1, String str2){
+        return 1;
+    }
+}
